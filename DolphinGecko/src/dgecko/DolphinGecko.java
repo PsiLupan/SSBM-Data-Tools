a9f3d4fea992afbcb46f64b8dc2dc49c3ded9500
PsiLupan <wcarter312@gmail.com>
2015-09-01 22:04:22 -0500

    IT'S ALIVE

--- DolphinGecko/src/dgecko/DolphinGecko.java
+++ DolphinGecko/src/dgecko/DolphinGecko.java
@@ -20,15 +20,14 @@ public class DolphinGecko {
 	
 	private enum GameStatus{
 		START_MATCH,
-		SEL_CHARS_AMOUNT,
-		SEL_CHARS,
+		SELECTED_CHARS,
 		IN_MATCH,
 		END_MATCH;
 	}
 	GameStatus statusMinor = GameStatus.START_MATCH;
 	
 	private String currMap = "0x0";
-	private byte[] characters;
+	private byte[] characters = new byte[4];;
 	
 	DolphinGecko() throws UnknownHostException, IOException{
 		short currChar = 0;
@@ -41,37 +40,30 @@ public class DolphinGecko {
 					System.out.println("Connected to Dolphin");
 					socket.getOutputStream().write((byte)0x99);
 					byte[] bytes = new byte[1];
-					bytes[0] = in.readByte();
+					in.read(bytes);
 					System.out.println("Gecko Console Version: " + DatatypeConverter.printHexBinary(bytes));
 					statusMajor = Status.CONNECTED;
 					break;
 				
 				case CONNECTED:
-					if(in.available() >= 1){
-						byte[] in_bytes = new byte[1];
-						in_bytes[0] = in.readByte();
+					if(in.available() > 0){
+						byte[] in_bytes = new byte[in.available()];
+						in.read(in_bytes);
 						String data = DatatypeConverter.printHexBinary(in_bytes);
 						switch(statusMinor){
 							case START_MATCH:
 								currMap = data;
 								System.out.println("Selected Map: " + currMap);
-								statusMinor = GameStatus.SEL_CHARS_AMOUNT;
+								statusMinor = GameStatus.SELECTED_CHARS;
 								break;
 						
-							case SEL_CHARS_AMOUNT:
-								System.out.println("Number of Characters: " + data);
-								characters = new byte[Integer.parseInt(data)];
-								statusMinor = GameStatus.SEL_CHARS;
-								break;
-						
-							case SEL_CHARS:
-								if(currChar < characters.length){
-									characters[currChar] = Byte.parseByte(data);
-									currChar += 1;
-								}else{
-									currChar = 0;
+							case SELECTED_CHARS:
+								System.out.println("Character: " + data);
+								characters[currChar] = in_bytes[0];
+								currChar += 1;
+								if(currChar > 1){ //TODO: Add some kind of ACK to support more than 2 characters
 									statusMinor = GameStatus.IN_MATCH;
-								}					
+								}
 								break;
 						
 							case IN_MATCH:
